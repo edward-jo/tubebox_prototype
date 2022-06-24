b07350fac889ef2f9627fb9d69f5314046dd5527
Edward Jo <helloedwardjo@gmail.com>
2022-06-24 20:14:19 +0900

    Test google signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'dart:developer' as developer;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tubebox_prototype/secure.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  clientId: webClientId,
+  scopes: <String>[
+    'https://www.googleapis.com/auth/youtube',
+    'https://www.googleapis.com/auth/youtube.readonly',
+  ],
+);
 
 void main() async {
   /// Load environment variables
@@ -31,11 +42,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((account) async {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        final authentication = await _currentUser!.authentication;
+        final accessToken = authentication.accessToken;
+        developer.log('access token: $accessToken');
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: Center(child: Text('Hello TubeBox\n$webClientId')),
+      body: Center(
+        child: TextButton(
+          onPressed: _signIn,
+          child: const Text('Sign In'),
+        ),
+      ),
     );
+  }
+
+  void _signIn() async {
+    await _googleSignIn.signIn();
   }
 }
