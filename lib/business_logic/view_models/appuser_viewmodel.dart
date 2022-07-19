bb137eb7cf929f987cb0897a896a7de7abc04a46
Edward Jo <helloedwardjo@gmail.com>
2022-07-19 18:14:11 +0900

    Add necessary methods(not implemented) to AppUserViewModel

--- lib/business_logic/view_models/appuser_viewmodel.dart
+++ lib/business_logic/view_models/appuser_viewmodel.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:tubebox_prototype/business_logic/models/appuser.dart';
 
-class AppUserViewModel extends ChangeNotifier {}
+class AppUserViewModel extends ChangeNotifier {
+  // signIn
+  Future<AppUser?> signIn() async {}
+
+  // signOut
+  Future<AppUser?> signOut() async {}
+
+  // isSignedIn
+  Future<bool> isSignedIn() async => true;
+}
