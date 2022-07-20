378d019017c093e13356ae02505866af64a7e1bb
Edward Jo <helloedwardjo@gmail.com>
2022-07-20 21:16:31 +0900

    Register AppUserViewModel as singleton service

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,8 +1,19 @@
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:tubebox_prototype/business_logic/view_models/appuser_viewmodel.dart';
+import 'package:tubebox_prototype/secure.dart';
 
 GetIt serviceLocator = GetIt.instance;
 
 void setupServiceLocator() {
-  serviceLocator.registerLazySingleton<GoogleSignIn>(() => GoogleSignIn());
+  serviceLocator.registerSingleton<GoogleSignIn>(
+    GoogleSignIn(
+      clientId: webClientId,
+      scopes: <String>[
+        'https://www.googleapis.com/auth/youtube',
+        'https://www.googleapis.com/auth/youtube.readonly',
+      ],
+    ),
+  );
+  serviceLocator.registerSingleton<AppUserViewModel>(AppUserViewModel());
 }
