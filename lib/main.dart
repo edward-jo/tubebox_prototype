8aa31c2b7d2980ed41c308002e12e8c0ce4c3f8e
Edward Jo <helloedwardjo@gmail.com>
2022-07-20 20:27:52 +0900

    Create service locator before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tubebox_prototype/services/service_locator.dart';
 import 'package:tubebox_prototype/ui/themes/theme.dart';
 import 'package:tubebox_prototype/ui/views/startup/startup_screen.dart';
 
 void main() async {
   /// Load environment variables
   await dotenv.load();
-
+  setupServiceLocator();
   runApp(const MyApp());
 }
 
