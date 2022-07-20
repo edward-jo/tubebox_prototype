370abfa8f23ebd8465113c7bd3bf7ccc8e8145b3
Edward Jo <helloedwardjo@gmail.com>
2022-07-20 21:17:50 +0900

    Implement signin method of AppUserViewModel

--- lib/business_logic/models/appuser.dart
+++ lib/business_logic/models/appuser.dart
@@ -1,1 +1,7 @@
-class AppUser {}
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AppUser {
+  AppUser(this.account);
+
+  GoogleSignInAccount account;
+}
--- lib/business_logic/view_models/appuser_viewmodel.dart
+++ lib/business_logic/view_models/appuser_viewmodel.dart
@@ -1,13 +1,25 @@
+import 'dart:developer' as developer;
+
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tubebox_prototype/business_logic/models/appuser.dart';
+import 'package:tubebox_prototype/services/service_locator.dart';
 
 class AppUserViewModel extends ChangeNotifier {
-  // signIn
-  Future<AppUser?> signIn() async {}
+  final _googleSignIn = serviceLocator<GoogleSignIn>();
+  AppUser? _appUser;
 
-  // signOut
+  Future<AppUser?> signIn() async {
+    GoogleSignInAccount? account = await _googleSignIn.signIn();
+    if (account == null) {
+      developer.log('Failed to sign in');
+      return null;
+    }
+    _appUser = AppUser(account);
+    return _appUser;
+  }
+
   Future<AppUser?> signOut() async {}
 
-  // isSignedIn
   Future<bool> isSignedIn() async => true;
 }
