72e8c5669cd2fc71dbc857f4c0837ea39f915290
Edward Jo <helloedwardjo@gmail.com>
2022-07-27 19:44:38 +0900

    Add authentication member to AppUser

--- lib/business_logic/models/appuser.dart
+++ lib/business_logic/models/appuser.dart
@@ -1,7 +1,8 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AppUser {
-  AppUser(this.account);
+  AppUser(this.account, this.authentication);
 
   GoogleSignInAccount account;
+  GoogleSignInAuthentication authentication;
 }
