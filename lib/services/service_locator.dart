d1a7d076be3c527f37f9155a47659d9128f00daf
Edward Jo <helloedwardjo@gmail.com>
2022-07-27 20:00:16 +0900

    Register YouTubeDataService and SubscriptionsViewModel to service locator

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,7 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tubebox_prototype/business_logic/view_models/appuser_viewmodel.dart';
+import 'package:tubebox_prototype/business_logic/view_models/subscriptions_viewmodel.dart';
 import 'package:tubebox_prototype/secure.dart';
+import 'package:tubebox_prototype/services/youtube_service/youtube_data_service.dart';
+import 'package:tubebox_prototype/services/youtube_service/youtube_data_service_impl.dart';
 
 GetIt serviceLocator = GetIt.instance;
 
@@ -15,5 +18,13 @@ void setupServiceLocator() {
       ],
     ),
   );
-  serviceLocator.registerSingleton<AppUserViewModel>(AppUserViewModel());
+  serviceLocator.registerSingleton<YouTubeDataService>(
+    YouTubeDataServiceImpl(),
+  );
+  serviceLocator.registerSingleton<AppUserViewModel>(
+    AppUserViewModel(),
+  );
+  serviceLocator.registerSingleton<SubscriptionsViewModel>(
+    SubscriptionsViewModel(),
+  );
 }
