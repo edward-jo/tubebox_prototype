986818ea197d9420dc4ba2790cd28c9323a2315b
Edward Jo <helloedwardjo@gmail.com>
2022-07-28 15:12:52 +0900

    Add 'Get Subscriptions' button for testing

--- lib/ui/views/main/main_screen.dart
+++ lib/ui/views/main/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tubebox_prototype/business_logic/view_models/appuser_viewmodel.dart';
+import 'package:tubebox_prototype/business_logic/view_models/subscriptions_viewmodel.dart';
 import 'package:tubebox_prototype/ui/views/signin/signin_screen.dart';
 
 class MainScreen extends StatefulWidget {
@@ -15,25 +16,46 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
+    final appUserVM = context.read<AppUserViewModel>();
+    final subscriptionVM = context.read<SubscriptionsViewModel>();
+    final appUser = appUserVM.appUser;
+    final accessToken = appUser?.authentication.accessToken;
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Placeholder(),
-          ElevatedButton(
-            onPressed: () async {
-              final ret = await context.read<AppUserViewModel>().signOut();
-              if (ret) {
-                if (!mounted) return;
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  SignInScreen.routeName,
-                  (route) => false,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(appUser?.account.displayName ?? ''),
+            Text(appUser?.authentication.accessToken ?? ''),
+            const SizedBox(height: 20),
+            // -- GET SUBSCRIPTIONS
+            ElevatedButton(
+              onPressed: () async {
+                await subscriptionVM.getSubscriptionList(
+                  accessToken!,
                 );
-              }
-            },
-            child: const Text('Google Sign Out'),
-          ),
-        ],
+              },
+              child: const Text('Get Subscriptions'),
+            ),
+            // --
+            const SizedBox(height: 20),
+            // -- Sign Out
+            ElevatedButton(
+              onPressed: () async {
+                final ret = await appUserVM.signOut();
+                if (ret) {
+                  if (!mounted) return;
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    SignInScreen.routeName,
+                    (route) => false,
+                  );
+                }
+              },
+              child: const Text('Google Sign Out'),
+            ),
+          ],
+        ),
       ),
     );
   }
