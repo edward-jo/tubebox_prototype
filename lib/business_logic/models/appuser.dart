6a31b61d77c496ad28787b5e395c47ffa603b80a
Edward Jo <helloedwardjo@gmail.com>
2022-07-28 20:59:54 +0900

    Refactor AppUser class - Inherited from ChangeNotifier - has listener for current user changed event

--- lib/business_logic/models/appuser.dart
+++ lib/business_logic/models/appuser.dart
@@ -1,8 +1,32 @@
+import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AppUser {
-  AppUser(this.account, this.authentication);
+class AppUser extends ChangeNotifier {
+  GoogleSignInAccount? _account;
 
-  GoogleSignInAccount account;
-  GoogleSignInAuthentication authentication;
+  GoogleSignInAccount? get account => _account;
+
+  set account(GoogleSignInAccount? account) {
+    _account = account;
+    notifyListeners();
+  }
+
+  GoogleSignInAuthentication? _authentication;
+
+  GoogleSignInAuthentication? get authentication => _authentication;
+
+  set authentication(GoogleSignInAuthentication? authentication) {
+    _authentication = authentication;
+  }
+
+  void currentUserChangedListener(GoogleSignInAccount? account) async {
+    if (account != null) {
+      this.account = account;
+      authentication = await account.authentication;
+    } else {
+      this.account = null;
+      authentication = null;
+    }
+    notifyListeners();
+  }
 }
